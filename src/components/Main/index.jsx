e23b9980bb83aedbe3fd9f6de8351f111cb2ea35
pvillor <pvillor@gmail.com>
2022-09-20 11:56:31 -0300

    fix/about_me

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -12,7 +12,7 @@ const Main = () => {
                     <Ore src="ore.jfif" alt="Paulo Victor" />
                     <TextsAboutMe>
                         <h2>Paulo Victor Guerra</h2>
-                        <p>Sempre fui ligado ao mundo dos games. Gosto da área de tecnologia porque é um universo gigante, cheio de possibilidades, a partir do qual muitas soluções de problemas foram e são desenvolvidas. Sou Desenvolvedor Front End, estudando Desenvolvimento Full Stack e Engenharia de Software. Gosto de aprender e pretendo aprimorar habilidades técnicas e interpessoais, com o intuito de solucionar problemas.</p>
+                        <p>Sempre fui ligado ao mundo dos games. Gosto da área de tecnologia porque é um universo gigante, cheio de possibilidades, a partir do qual muitas soluções de problemas foram e são desenvolvidas. Sou Desenvolvedor Full Stack e estou me graduando em Engenharia de Software. Gosto de aprender e pretendo aprimorar habilidades técnicas e interpessoais, com o intuito de solucionar problemas.</p>
                     </TextsAboutMe>
                 </AboutMe>
             </section>
